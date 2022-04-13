544026f8e91232e726a1b498b44f10c07151fab4
dakara-mara <103514085+dakara-mara@users.noreply.github.com>
2022-04-13 11:37:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 
 import streamlit as s
+import pandas
 
 s.title("test")
 s.header("The header")
 s.text("Lorem ipsum dolor sin amet.")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
